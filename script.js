2c41d332cf47e031d43d75363b2e3bb721f1ab3a
LorenzoF2008 <renzobustamante87@gmail.com>
2025-12-04 10:59:50 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("inizio esecuzione");
+function elabora(){
+let numero = document.getElementById("inNumber").value;
+alert(numero);
+if("inNumber".value<=255){
+    alert("giusto");    
+    }else{
+    alert("sbagliato");    
+    }
+}

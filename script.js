b4322d235a973ba0757cae31b16b5935a4d79203
LorenzoF2008 <renzobustamante87@gmail.com>
2025-12-04 11:37:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ if("inNumber".value<=255){
     alert("sbagliato");    
     }
 }
+
+console.log("inizio esecuzione");
+let d = new Date();
+console.log(d);
+document.getElementById("saluto").innerHTML=d+"è un bel giorno";
+function elabora(){
+let numero = document.getElementById("inNumber").value;
+alert(numero);
+if(numero >=0 && numero <=255){
+    document.getElementById("out").innerHTML="Hai inserito il numero"+numero;   
+    }
+    else{
+    document.getElementById("error").innerHTML ="il numero non è corretto";   
+   
+    }
+}
